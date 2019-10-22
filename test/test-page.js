3cb36fabc2ef016e222debef0f1f8c024d1b56f7
unknown <poxx0003@stud.kea.dk>
2019-10-22 13:45:59 +0200

    Unit test for purchase.js

--- test/test-page.js
+++ test/test-page.js
@@ -4,7 +4,7 @@ const assert = require('assert');
 
 const PurchaseClass = require('../classes/purchase');
 
-
+/*
 describe('Page status test', function() {
     it('Main page status', function(done) {
         request('http://localhost:3030' , function(error, response, body) {
@@ -13,7 +13,7 @@ describe('Page status test', function() {
         });
     });
 });
-
+*/
 describe('Test setting internet-connection', function() {
     it('Test if the internetconnection boolean is false', function() {
         const thisPurchase = new PurchaseClass(false, 0, [], 0);
@@ -32,4 +32,87 @@ describe('Test setting internet-connection', function() {
         assert.equal(thisPurchase.totalPrice, 200);
     });
 
+});
+
+describe('Test setting phone lines', function() {
+    // Upper boundary tests
+    it('Adding the 8th phone line', function() {
+        const thisPurchase = new PurchaseClass(false, 7, [], 0);
+        thisPurchase.addPhoneLine()
+        assert.equal(thisPurchase.phoneLines, 8)
+    })
+    it('Do not add the 9th phone line', function() {
+        const thisPurchase = new PurchaseClass(false, 8, [], 0);
+        thisPurchase.addPhoneLine()
+        assert.equal(thisPurchase.phoneLines, 8)
+    })
+
+    // Lower boundary tests
+    it('Removing 1 phone line, when there is only 1', function() {
+        const thisPurchase = new PurchaseClass(false, 1, [], 0);
+        thisPurchase.subtractPhoneLine()
+        assert.equal(thisPurchase.phoneLines, 0)
+    })
+    it('Try to remove 1 phone line, when there is 0', function() {
+        const thisPurchase = new PurchaseClass(false, 0, [], 0);
+        thisPurchase.subtractPhoneLine()
+        assert.equal(thisPurchase.phoneLines, 0)
+    })
+})
+
+describe('Test adding and removing cellphone functions', function () {
+    // Lower boundary tests?
+    it('Adding an iPhone 99 when array is empty', function() {
+        phoneModel = 'iPhone 99'
+        const thisPurchase = new PurchaseClass(false, 0, [], 0)
+        thisPurchase.addCellPhone(phoneModel)
+        assert.equal(thisPurchase.cellPhones[0], phoneModel)
+    })
+    it('Adding an unknown phone when array is empty', function() {
+        phoneModel = 'iPhone 99999'
+        const thisPurchase = new PurchaseClass(false, 0, [], 0)
+        thisPurchase.addCellPhone(phoneModel)
+        assert.equal(thisPurchase.cellPhones.indexOf(phoneModel), -1)
+    })
+    it('Remove iPhone 99 when array is empty', function() {
+        phoneModel = 'iPhone 99'
+        const thisPurchase = new PurchaseClass(false, 0, [], 0)
+        thisPurchase.removeCellPhone(phoneModel)
+        //assert.equal(thisPurchase.cellPhones.length, 0)
+        assert.deepEqual(thisPurchase.cellPhones, [])
+    })
+    it('Try removing iPhone 99 when array only contains Huawei 99', function() {
+        phoneModelToRemove = 'iPhone 99'
+        phoneModelAdded = 'Huawei 99'
+        const thisPurchase = new PurchaseClass(false, 0, [phoneModelAdded], 0)
+        thisPurchase.removeCellPhone(phoneModelToRemove)
+        assert.equal(thisPurchase.cellPhones[0], phoneModelAdded)
+    })
+
+    it('Adding all known phone models', function() {
+        phoneModelToAdd = ['Motorola G99', 'iPhone 99', 'Samsung Galaxy 99', 'Sony Xperia 99', 'Huawei 99']
+        const thisPurchase = new PurchaseClass(false, 0, [], 0)
+        for(let i = 0; i < phoneModelToAdd.length; i++){
+            thisPurchase.addCellPhone(phoneModelToAdd[i])
+        }
+        assert.deepEqual(thisPurchase.cellPhones, phoneModelToAdd)     
+    })
+
+    it('Adding multiple copies of same phone models', function() {
+        phoneModelToAdd = ['Motorola G99', 'iPhone 99', 'Samsung Galaxy 99', 'Sony Xperia 99', 'Huawei 99']
+        const thisPurchase = new PurchaseClass(false, 0, [], 0)
+        for(let j = 0; j < 2; j++){
+            for(let i = 0; i < phoneModelToAdd.length; i++){
+                thisPurchase.addCellPhone(phoneModelToAdd[i])
+            }
+        }
+        assert.deepEqual(thisPurchase.cellPhones, phoneModelToAdd.concat(phoneModelToAdd))     
+    })
+
+})
+describe('Testing the checkout function', function() {
+    it('Check if string contains \'Internet connection\'', function() {
+        const thisPurchase = new PurchaseClass(true, 0, [], 0)
+        assert.equal(thisPurchase.checkout().includes('Internet connection'), true)
+    });
 });

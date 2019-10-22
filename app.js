93e235fea69c338c7659f2f3a578a8432cad0d12
Jens Bäckvall <jens@entropyfox.com>
2019-10-22 11:39:22 +0200

    change paths for app within public

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const app = express();
 const bodyparser = require('body-parser');
 app.use(bodyparser.urlencoded({ extended: true }));
 
+app.use(express.static('public'));
+
 const purchase = require('./classes/purchase');
 
 app.listen(3030,function(err){
@@ -11,5 +13,5 @@ app.listen(3030,function(err){
 
 
 app.get('/', (req,res)=>{
-    res.sendFile(__dirname+"/index.html")
+    res.sendFile(__dirname+"/public/html/index.html")
 });
